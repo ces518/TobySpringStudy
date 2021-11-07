c7f442762e01244ca9bcbdb40d7f7aa6130094f5
me <me.ncucu@kakaocorp.com>
2021-11-07 21:32:34 +0900

    [07] 스프링 핵심 기술의 응용 - UserDao 와 SQL 분리 : Jaxb

---
+++ [07]_스프링_핵심_기술_응용/src/test/java/jaxb/JaxbTest.java
@@ -0,0 +1,36 @@
+package jaxb;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import org.junit.jupiter.api.Test;
+import sqlservice.jaxb.SqlType;
+import sqlservice.jaxb.Sqlmap;
+
+public class JaxbTest {
+
+    @Test
+    void readSqlMap() throws Exception {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        JAXBContext context = JAXBContext.newInstance(contextPath);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Sqlmap sqlMap = (Sqlmap) unmarshaller.unmarshal(
+            getClass().getResourceAsStream("sqlmap.xml")
+        );
+
+        List<SqlType> sqlList = sqlMap.getSql();
+
+        assertThat(sqlList.size()).isEqualTo(3);
+        assertThat(sqlList.get(0).getKey()).isEqualTo("add");
+        assertThat(sqlList.get(0).getValue()).isEqualTo("insert");
+
+        assertThat(sqlList.get(1).getKey()).isEqualTo("get");
+        assertThat(sqlList.get(1).getValue()).isEqualTo("select");
+
+        assertThat(sqlList.get(2).getKey()).isEqualTo("delete");
+        assertThat(sqlList.get(2).getValue()).isEqualTo("delete");
+    }
+}
