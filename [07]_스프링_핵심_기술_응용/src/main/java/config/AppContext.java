55b7e9ca257d391d51c0b6646cd6cc41333274c5
me <me.ncucu@kakaocorp.com>
2021-11-09 18:17:12 +0900

    [07] 스프링 핵심 기술의 응용 - PropertySourcePlaceHolderConfigurer

--- [07]_스프링_핵심_기술_응용/src/main/java/config/AppContext.java
+++ [07]_스프링_핵심_기술_응용/src/main/java/config/AppContext.java
@@ -6,11 +6,13 @@ import domain.UserLevelUpgradePolicy;
 import java.sql.Driver;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
@@ -24,25 +26,28 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @PropertySource("classpath:database.properties")
 public class AppContext {
 
-    @Autowired
-    Environment env;
+    @Value("${db.driverClass}")
+    Class<? extends Driver> driverClass;
+
+    @Value("${db.url}")
+    String url;
+
+    @Value("${db.username}")
+    String username;
+
+    @Value("${db.password}")
+    String password;
 
     @Autowired
     UserDao userDao;
 
-
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        try {
-            dataSource.setDriverClass(
-                (Class<? extends Driver>) Class.forName(env.getProperty("db.driverClass")));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.username"));
-        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setDriverClass(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
@@ -54,5 +59,15 @@ public class AppContext {
     @Bean
     public UserLevelUpgradePolicy userLevelUpgradePolicy() {
         return new DefaultUserLevelUpgradePolicy();
+    }
+
+    /**
+     * 반드시 static 메소드로 선언해야함
+     * BeanFactoryPostProcessor 타입의 빈은 반드시 static 이여야 한다..
+     * https://mangkyu.tistory.com/177
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
