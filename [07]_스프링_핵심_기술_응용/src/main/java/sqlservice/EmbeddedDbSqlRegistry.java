71a7bb7ce249d8fb7898c3c6bae9456b99e2440a
me <me.ncucu@kakaocorp.com>
2021-11-09 14:53:59 +0900

    [07] 스프링 핵심 기술의 응용 - UserDao 와 SQL 분리 : EmbeddedDbSqlRegistry, Transaction 적용

--- [07]_스프링_핵심_기술_응용/src/main/java/sqlservice/EmbeddedDbSqlRegistry.java
+++ [07]_스프링_핵심_기술_응용/src/main/java/sqlservice/EmbeddedDbSqlRegistry.java
@@ -8,13 +8,21 @@ import javax.sql.DataSource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 
     JdbcTemplate template;
+    TransactionTemplate transactionTemplate;
 
     public void setDataSource(DataSource dataSource) {
         template = new JdbcTemplate(dataSource);
+        transactionTemplate = new TransactionTemplate(
+            new DataSourceTransactionManager(dataSource)
+        );
     }
 
     @Override
@@ -43,10 +51,21 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
         }
     }
 
+    /**
+     * 일반적으로는 트랜잭션 관련 객체를 싱글톤 빈으로 등록해야 사용한다.
+     * 하지만 적용범위가 EmbeddedDbSqlRegistry 처럼 협소한경우 TransactionTemplate 을 사용하는 방식도 있다.
+     * 또한 특정 메소드에서만 적용할 것이기 때문에 TransactionTemplate 을 직접 생성해서 사용하는 것이 낫다.
+     */
     @Override
     public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-        for (Entry<String, String> entry : sqlmap.entrySet()) {
-            updateSql(entry.getKey(), entry.getValue());
-        }
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
     }
 }
--- [07]_스프링_핵심_기술_응용/src/test/java/sqlservice/EmbeddedDbSqlRegistryTest.java
+++ [07]_스프링_핵심_기술_응용/src/test/java/sqlservice/EmbeddedDbSqlRegistryTest.java
@@ -1,6 +1,12 @@
 package sqlservice;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import errors.SqlUpdateFailureException;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -19,6 +25,21 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
         EmbeddedDbSqlRegistry registry = new EmbeddedDbSqlRegistry();
         registry.setDataSource(db);
         return registry;
+    }
+
+    @Test
+    void transactionalUpdate() {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY99999!@#$", "Modified9999");
+
+        try {
+            registry.updateSql(sqlmap);
+            fail();
+        } catch (SqlUpdateFailureException ignored) {}
+        checkFindResult("SQL1", "SQL2", "SQL3");
     }
 
     @AfterEach
