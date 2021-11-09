100b661e6b13254dac23758dea66ddf0309320ea
me <me.ncucu@kakaocorp.com>
2021-11-09 18:12:14 +0900

    [07] 스프링 핵심 기술의 응용 - PropertySource

--- [07]_스프링_핵심_기술_응용/src/main/java/config/AppContext.java
+++ [07]_스프링_핵심_기술_응용/src/main/java/config/AppContext.java
@@ -3,12 +3,15 @@ package config;
 import dao.UserDao;
 import domain.DefaultUserLevelUpgradePolicy;
 import domain.UserLevelUpgradePolicy;
+import java.sql.Driver;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -18,18 +21,28 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "dao")
 @Import({SqlServiceContext.class, ProductionAppContext.class})
+@PropertySource("classpath:database.properties")
 public class AppContext {
+
+    @Autowired
+    Environment env;
 
     @Autowired
     UserDao userDao;
 
+
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/spring_batch");
-        dataSource.setUsername("root");
-        dataSource.setPassword("password");
+        try {
+            dataSource.setDriverClass(
+                (Class<? extends Driver>) Class.forName(env.getProperty("db.driverClass")));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
 
