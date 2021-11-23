9cb23fd01c8619e33d5a89244e412eda2a2bea4c
me <me.ncucu@kakaocorp.com>
2021-11-23 23:11:15 +0900

    [08] Ioc 컨테이너와 DI - GenericApplicationContext

--- [08]_IoC_컨테이너와_DI/src/test/java/me/june/AppTest.java
+++ [08]_IoC_컨테이너와_DI/src/test/java/me/june/AppTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
 
 public class AppTest {
@@ -52,6 +54,24 @@ public class AppTest {
         helloDef.getPropertyValues().addPropertyValue("printer", new RuntimeBeanReference("printer"));
 
         applicationContext.registerBeanDefinition("hello", helloDef);
+
+        Hello hello = applicationContext.getBean("hello", Hello.class);
+        hello.print();
+
+        assertThat(applicationContext.getBean("printer").toString(), is("Hello Spring"));
+    }
+
+    @Test
+    void genericApplicationContext() throws Exception {
+        // GenericApplicationContext 는 파일이나, 애노테이션 등 외부리소스를 이용해 설정정보를 읽어온다.
+        // 이때 BeanDefinitionReader 를 사용한다.
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+
+        // XmlBeanDefinitionReader 은 기본적으로 클래스패스에서 리소스를 찾는다.
+        // classpath:, file: http: 같은 접두어로 리소스 타입 지정도 가능하다.
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(applicationContext);
+        reader.loadBeanDefinitions("genericApplicationContext.xml");
+        applicationContext.refresh(); // 메타 정보 등록후 애플리케이션 컨텍스트 초기화
 
         Hello hello = applicationContext.getBean("hello", Hello.class);
         hello.print();
