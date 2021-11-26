c4f416e58e0df32f6fb089dff1594cba4a13179a
me <me.ncucu@kakaocorp.com>
2021-11-26 23:57:55 +0900

    [08] Ioc 컨테이너와 DI - ComponentScan

---
+++ [08]_IoC_컨테이너와_DI/src/main/java/me/june/config/AppConfig.java
@@ -0,0 +1,19 @@
+package me.june.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+    basePackages = "me.june",
+    excludeFilters = {
+        @Filter(Configuration.class), // @Configuration 클래스를 필터 대상에서 제외
+        @Filter(type = FilterType.ASSIGNABLE_TYPE, value = AppConfig.class) // FilterType.ASSIGNABLE_TYPE 지정해 특정 타입을 대상으로 할 수 있다.
+                                                                            // AppConfig 클래스를 제외
+    }
+)
+public class AppConfig {
+
+}
