1424767fe9d2e6c165fc03ac832cf3805b7ead44
me <me.ncucu@kakaocorp.com>
2021-11-30 13:12:44 +0900

    [09] 데이터 엑세스 기술 - IBatis : SqlMapClientDaoSupport

--- [09]_데이터_엑세스_기술/src/main/java/me/june/ibatis/IBatisMemberDao.java
+++ [09]_데이터_엑세스_기술/src/main/java/me/june/ibatis/IBatisMemberDao.java
@@ -1,13 +1,14 @@
 package me.june.ibatis;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
+import me.june.Member;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
-public class IBatisMemberDao {
+public class IBatisMemberDao extends SqlMapClientDaoSupport {
 
-    SqlMapClientTemplate sqlMapClientTemplate;
-
-    public void setSqlMapClientTemplate(SqlMapClient sqlMapClient) {
-        this.sqlMapClientTemplate = new SqlMapClientTemplate(sqlMapClient);
+    public void insert(Member member) {
+        getSqlMapClientTemplate().insert("insertMember", member);
     }
+
 }
