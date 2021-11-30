55f7045c5f2ea2f2f1a496df64aa50a48e66daf1
me <me.ncucu@kakaocorp.com>
2021-11-30 13:15:23 +0900

    [09] 데이터 엑세스 기술 - IBatis : insert/update/delete

--- [09]_데이터_엑세스_기술/src/main/java/me/june/ibatis/IBatisMemberDao.java
+++ [09]_데이터_엑세스_기술/src/main/java/me/june/ibatis/IBatisMemberDao.java
@@ -11,4 +11,13 @@ public class IBatisMemberDao extends SqlMapClientDaoSupport {
         getSqlMapClientTemplate().insert("insertMember", member);
     }
 
+    public void update(Member member) {
+        getSqlMapClientTemplate().update("updateMember", member, 1);
+        // requiredRowAffected 인자가 있는 메소드 사용시, 기대 값과 결과가 일치하지 않는다면, JdbcUpdateAffectedIncorrectNumberOfRowsException 예외가 발생한다.
+    }
+
+    public void delete(Long id) {
+        getSqlMapClientTemplate().delete("deleteMember", id, 1);
+        // requiredRowAffected 인자가 있는 메소드 사용시, 기대 값과 결과가 일치하지 않는다면, JdbcUpdateAffectedIncorrectNumberOfRowsException 예외가 발생한다.
+    }
 }
