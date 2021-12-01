7c6e2f3731d8db30b733f4f7ea24993fab597adf
me <me.ncucu@kakaocorp.com>
2021-12-01 23:25:25 +0900

    [09] 데이터 엑세스 기술 - Transaction : DataSourceTransactionManager / TransactionAwareDataSourceProxy

--- [09]_데이터_엑세스_기술/src/main/java/me/june/ibatis/IbatisConfig.java
+++ [09]_데이터_엑세스_기술/src/main/java/me/june/ibatis/IbatisConfig.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.orm.ibatis.SqlMapClientFactoryBean;
 
 @Configuration
@@ -20,6 +22,28 @@ public class IbatisConfig {
         sqlMapClientFactoryBean.setDataSource(dataSource());
         sqlMapClientFactoryBean.setConfigLocation(resourceLoader.getResource("classpath:ibatis/SqlMapConfig.xml"));
         return sqlMapClientFactoryBean;
+    }
+
+    /**
+     * JDBC Connection API 를 활용해 트랜잭션을 제어한다.
+     * JDBC / Ibatis 에 적용할 수 있다.
+     * DataSourceTransactionManager 가 사용할 DataSource 는 getConnection() 호훛ㄹ시 마다 새로운 커넥션을 제공해야 한다.
+     * threadlocal 등을 활용해 저장해 두고 사용하는 DataSource 등을 사용해선 안된다.
+     */
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    /**
+     * 레거시에서 사용하려면 TransactionAwareDataSourceProxy 를 적용하거나 DataSourceUtils 를 이용 해야 한다.
+     * @see TransactionAwareDataSourceProxy
+     */
+    @Bean
+    public TransactionAwareDataSourceProxy dataSourceProxy() {
+        TransactionAwareDataSourceProxy transactionAwareDataSourceProxy = new TransactionAwareDataSourceProxy();
+        transactionAwareDataSourceProxy.setTargetDataSource(dataSource());
+        return transactionAwareDataSourceProxy;
     }
 
     @Bean
