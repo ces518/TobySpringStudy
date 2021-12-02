a40ec56c91c62f7f4143b96cad62c7ce3aebbb8f
me <me.ncucu@kakaocorp.com>
2021-12-02 23:07:26 +0900

    [10] 스프링 웹기술과 스프링 MVC - MockServletContext

--- [10]_스프링_웹기술과_스프링MVC/src/test/java/me/june/mvc/MockServletTest.java
+++ [10]_스프링_웹기술과_스프링MVC/src/test/java/me/june/mvc/MockServletTest.java
@@ -19,6 +19,9 @@ public class MockServletTest {
         /**
          * 서블릿에 전달할 HttpServletRequest 타입의 요청정보를 구성하게 해준다.
          * 세션 정보가필요하다면, MockHttpSession 을 사용하면 된다.
+         *
+         * 드물게 ServletConfig/ServletContext 를 별도로 만들어 테스트해야 하는 경우가 있다.
+         * MockServletContext 를 생성해 스프링 루트 컨텍스트를 넣어주는 등 활용이 가능하다.
          */
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "/hello");
         request.addParameter("name", "Spring");
