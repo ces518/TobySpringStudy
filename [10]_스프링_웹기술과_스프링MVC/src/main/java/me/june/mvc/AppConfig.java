4f60f55e8a2dc606ffe6f5feae152474f4811b85
me <me.ncucu@kakaocorp.com>
2021-12-03 00:58:02 +0900

    [10] 스프링 웹기술과 스프링 MVC - SimpleUrlHandlerMapping

--- [10]_스프링_웹기술과_스프링MVC/src/main/java/me/june/mvc/AppConfig.java
+++ [10]_스프링_웹기술과_스프링MVC/src/main/java/me/june/mvc/AppConfig.java
@@ -2,6 +2,9 @@ package me.june.mvc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+
+import java.util.Properties;
 
 @Configuration
 public class AppConfig {
@@ -20,4 +23,15 @@ public class AppConfig {
         return new HelloController();
     }
 
+    /**
+     * SimpleUrlHandlerMapping 매핑정보를 한곳에 모아볼 수 있다.
+     */
+    @Bean
+    public SimpleUrlHandlerMapping simpleUrlHandlerMapping() {
+        SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
+        Properties properties = new Properties();
+        properties.put("/hello", "helloController");
+        handlerMapping.setMappings(properties);
+        return handlerMapping;
+    }
 }
