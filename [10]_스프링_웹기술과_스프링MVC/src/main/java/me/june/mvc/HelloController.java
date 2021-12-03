698d78b17fd772b555855912a3a41248c58e10b0
me <me.ncucu@kakaocorp.com>
2021-12-03 14:47:43 +0900

    [10] 스프링 웹기술과 스프링 MVC - InternalResourceView

--- [10]_스프링_웹기술과_스프링MVC/src/main/java/me/june/mvc/HelloController.java
+++ [10]_스프링_웹기술과_스프링MVC/src/main/java/me/june/mvc/HelloController.java
@@ -3,6 +3,7 @@ package me.june.mvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +24,12 @@ public class HelloController implements Controller {
         HashMap<String, Object> model = new HashMap<>();
         model.put("message", message);
 
-        return new ModelAndView("/WEB-INF/view/hello.jsp", model);
+        /**
+         * InternalResourceView 는 RequestDispatcher 의 forward/include 를 이용하는 뷰다.
+         * 주로 JSP 뷰를 만들때 활용한다.
+         */
+        InternalResourceView view = new InternalResourceView("/WEB-INF/view/hello.jsp");
+
+        return new ModelAndView(view, model);
     }
 }
