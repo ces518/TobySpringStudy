d1ab1b3db32572ee9965b6556da350c624481554
me <me.ncucu@kakaocorp.com>
2021-12-03 14:52:52 +0900

    [10] 스프링 웹기술과 스프링 MVC - RedirectView

--- [10]_스프링_웹기술과_스프링MVC/src/main/java/me/june/mvc/HelloController.java
+++ [10]_스프링_웹기술과_스프링MVC/src/main/java/me/june/mvc/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +30,12 @@ public class HelloController implements Controller {
          * 주로 JSP 뷰를 만들때 활용한다.
          */
         InternalResourceView view = new InternalResourceView("/WEB-INF/view/hello.jsp");
+
+        /**
+         * RedirectView 는 리다이렉트 처리를 해주는 뷰이다.
+         * 컨트롤러에서 이를 직접 만들기보단 redirect: 키워드를 활용해 뷰네임을 반환해주는 것이 좋다.
+         */
+        RedirectView redirectView = new RedirectView("/wow");
 
         return new ModelAndView(view, model);
     }
