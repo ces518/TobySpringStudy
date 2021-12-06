bd8a8991e910750b67fb87be2082d98bc33f075e
me <me.ncucu@kakaocorp.com>
2021-12-06 21:43:24 +0900

    [11] 스프링 MVC : Validator - JSR-303

--- [11]_스프링MVC/src/main/java/me/june/mvc/validator/User.java
+++ [11]_스프링MVC/src/main/java/me/june/mvc/validator/User.java
@@ -1,9 +1,19 @@
 package me.june.mvc.validator;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * JSR 303 애노테이션을 활용한 벨리데이션 하려면 LocalValidatorFactoryBean 이 빈으로 등록되어 있어야 한다.
+ */
 public class User {
 
     int id;
+
+    @Min(0)
     int age;
+
+    @NotNull
     String name;
 
     public int getId() {
