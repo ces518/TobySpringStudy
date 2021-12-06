7d99b5a399962debf4ab5cfc3ed50e6001503111
me <me.ncucu@kakaocorp.com>
2021-12-06 22:42:00 +0900

    [11] 스프링 MVC : WebArgumentResolver

---
+++ [11]_스프링MVC/src/main/java/me/june/mvc/argumentresolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,22 @@
+package me.june.mvc.argumentresolver;
+
+import me.june.mvc.User;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebArgumentResolver;
+import org.springframework.web.context.request.NativeWebRequest;
+
+/**
+ * WebArgumentResolver 를 활용하면 애플리케이션에 특화된 파라미터 리졸버를 만들 수 있다.
+ */
+public class CurrentUserArgumentResolver implements WebArgumentResolver {
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, NativeWebRequest webRequest)
+        throws Exception {
+        if (!methodParameter.hasMethodAnnotation(CurrentUser.class)) {
+            return WebArgumentResolver.UNRESOLVED;
+        }
+        // ... CurrentUser 세팅
+        return new User();
+    }
+}
