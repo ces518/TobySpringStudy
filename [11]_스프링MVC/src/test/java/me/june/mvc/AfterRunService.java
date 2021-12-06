3490187ffbc66ec0169930c7451f774d0f43c2c2
me <me.ncucu@kakaocorp.com>
2021-12-06 13:13:51 +0900

    [11] 스프링 MVC : SessionAttributesTest

---
+++ [11]_스프링MVC/src/test/java/me/june/mvc/AfterRunService.java
@@ -0,0 +1,20 @@
+package me.june.mvc;
+
+import java.io.UnsupportedEncodingException;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+public interface AfterRunService {
+
+    String getContentAsString() throws UnsupportedEncodingException;
+
+    WebApplicationContext getContext();
+
+    <T> T getBean(Class<T> beanType);
+
+    ModelAndView getModelAndView();
+
+    AfterRunService assertViewName(String viewName);
+
+    AfterRunService assertModel(String name, Object value);
+}
