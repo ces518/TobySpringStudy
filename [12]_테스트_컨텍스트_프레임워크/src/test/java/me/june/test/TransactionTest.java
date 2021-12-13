e134723b5b8c572c8333b8adc9c761ae653e490e
me <me.ncucu@kakaocorp.com>
2021-12-13 20:12:29 +0900

    [12] 테스트 컨텍스트 프레임워크 : TransactionTemplate

--- [12]_테스트_컨텍스트_프레임워크/src/test/java/me/june/test/TransactionTest.java
+++ [12]_테스트_컨텍스트_프레임워크/src/test/java/me/june/test/TransactionTest.java
@@ -4,11 +4,18 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class TransactionTest {
 
     @Autowired
     JpaDao dao;
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
 
     /**
      * 트랜잭션이 없는 단독 DAO 테스트, JdbcTemplate 은 문제가 없지만, JPA/Hibernate 의 경우 문제가 된다.
@@ -17,6 +24,21 @@ public class TransactionTest {
     @Test
     void query() {
         List<User> users = dao.findUsers();
+    }
+
+    /**
+     * 롤백 테스트를 위해 TransactionTemplate 을 사용하는 방법
+     */
+    @Test
+    void transactionTemplate() {
+        new TransactionTemplate(transactionManager).execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                status.setRollbackOnly();
+                // .. DB 작업
+                return null;
+            }
+        });
     }
 
     static class JpaDao {
