fb19212dc0bcdb42ee5be4faaca4c4028ed90978
me <me.ncucu@kakaocorp.com>
2021-12-13 20:35:43 +0900

    [12] 테스트 컨텍스트 프레임워크 : ContextConfiguration

---
+++ [12]_테스트_컨텍스트_프레임워크/src/main/java/me/june/test/AppConfig.java
@@ -0,0 +1,8 @@
+package me.june.test;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ [12]_테스트_컨텍스트_프레임워크/src/test/java/me/june/test/SpringContextTest.java
@@ -0,0 +1,21 @@
+package me.june.test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Spring 3.1 버전 부터 자바 설정을 활용한 테스트 컨텍스트 설정도 가능해졌다.
+ * 아무런 값을 지정하지 않은 경우에는, 이전과 동일하게 -context.xml 파일을 먼저 찾는다.
+ * 디폴트 설정 클래스를 찾는 방법은 테스트 클래스의 내부에 정의된 스태틱 멤버 클래스 중 @Configuration 이 붙은 것들을 모두 디폴트 설정 정보로 활용한다.
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class SpringContextTest {
+
+    @Configuration
+    static class TestConfig {
+
+    }
+}
